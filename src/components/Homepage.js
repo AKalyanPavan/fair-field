6d0e1abb2e3d71398531effa0852bfc339a458dc
AKalyanPavan <pavanaravelli690@gmail.com>
2025-03-26 13:20:42 +0530

    First Cut

---
+++ src/components/Homepage.js
@@ -0,0 +1,137 @@
+import Header from './Header.js'
+import Card from './Card.js'
+import LocationCard from './LocationCard.js'
+
+import firstSection from '../images/first-section.png'
+import poshBuilding from '../images/posh-building.png'
+
+import { basicFeatures, premiumAmenities, dreamHomeItems, locationList } from './Variables.js'
+
+import phone from '../images/phone.svg';
+
+export default function Homepage() {
+	return(
+		<div className="max-w-[1300px] mx-auto sm:px-[50px] px-[20px] font-['Lexend']">
+			<Header />
+			<div className="sm:mt-[150px] mt-[50px] sm:flex justify-between items-center sm:gap-[50px]">
+				<div className="text-[#37405E] min-[1200px]:max-w-[600px]">
+					<div className="text-[40px] sm:text-[54px] leading-[60px]">
+						<span>Welcome to Fair Field </span>
+						<br className="max-sm:hidden" />
+						<span>– Anicham & Magilam</span>
+					</div>
+					<div className="text-[#D3AC4A] text-[24px] my-[30px]">
+						Premium Gated Community in Coimbatore
+					</div>
+					<div className="text-[20px] font-light">
+						Experience the perfect blend of elegance, security, and future-ready living at Fair Field – Anicham & Magilam. Strategically located in Ramanathapuram, Coimbatore, our premium gated community offers world-class amenities, sustainable infrastructure, and unparalleled investment opportunities.
+					</div>
+					<div className="w-fit mt-[30px] bg-[#37405E] py-[10px] px-[20px] rounded-[5px] cursor-pointer">
+						<span className="text-[#ffffff] tracking-tight">
+							Book Your Visit Today
+						</span>
+					</div>
+				</div>
+				<div>
+					<img src={firstSection} className="max-[1200px]:hidden" />
+				</div>
+			</div>
+			<div className="mt-[100px] text-[#37405E] text-center">
+				<div className="sm:text-[40px] text-[30px]">Why Choose Fair Field?</div>
+				<div className="text-[20px] font-light my-[50px] max-w-[920px] mx-auto">
+					Situated in a well-connected neighborhood, offering easy access to business districts, educational institutions, and healthcare facilities.
+				</div>
+				<div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-[50px]">
+					{basicFeatures.map((basicFeautre, index) => (
+						<Card cardData={basicFeautre} />
+					))}
+				</div>
+				<div className="w-fit mt-[30px] bg-[#37405E] mt-[50px] py-[10px] px-[20px] rounded-[5px] cursor-pointer mx-auto">
+					<span className="text-[#ffffff] tracking-tight">
+						Join an Elite Community – Reserve Your Spot Today!
+					</span>
+				</div>
+			</div>
+			<div className="mt-[100px] text-[#37405E] text-center">
+				<div className="sm:text-[40px] text-[30px]">Our Premium Amenities</div>
+				<div className="mt-[50px] grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-[50px]">
+					{premiumAmenities.map((premiumAmenity, index) => (
+						<Card cardData={premiumAmenity} />
+					))}
+				</div>
+			</div>
+			<div className="mt-[100px] text-[#37405E] text-center">
+				<div className="sm:text-[40px] text-[30px]">Location & Accessibility</div>
+				<div className="mt-[50px] grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-[50px]">
+					{locationList.map((locationItem, index) => (
+						<div>
+							<LocationCard locationData={locationItem} />
+						</div>
+					))}
+				</div>
+			</div>
+			<div className="mt-[100px]">
+				<div className="flex gap-[50px] items-center justify-between">
+					<div className="min-[1200px]:max-w-[620px]">
+						<div className="sm:text-[40px] text-[30px] text-[#37405E]">Book Your Exclusive Site Visit</div>
+						<div className="text-[#7B7B7B] leading-[30px] mt-[10px]">
+							<span>Discover your future home today! We invite you to explore Fair Field</span>
+							<br />
+							<span>– Anicham & Magilam and witness luxury living firsthand.</span>
+						</div>
+						<div className="mt-[50px]">
+							<div className="text-[20px] text-[#37405E]">What You Get?</div>
+							<div className="flex flex-col gap-[10px] mt-[10px] text-[#7B7B7B] ml-[20px]">
+								<div className="flex">
+									<div> ✔ </div>
+									<div className="ml-[10px]">
+										Guided Tour – Walkthrough of our world-class gated community. Get a detailed look at the community layout, plot options, and exclusive features.
+									</div>
+								</div>
+								<div className="flex">
+									<div> ✔ </div>
+									<div className="ml-[10px]">
+										Personal Consultation – Speak with our real estate experts who will assist you in selecting the perfect home or plot based on your preferences. Clarify all queries related to pricing, financing, and legal aspects.
+									</div>
+								</div>
+								<div className="flex">
+									<div> ✔ </div>
+									<div className="ml-[10px]">
+										Exclusive Site-Visit Offers – Enjoy special discounts and deals available only for site visit attendees. Make the most of your visit by securing the best price on your dream home.
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div>
+						<img src={poshBuilding} className="rounded-[20px] max-[1200px]:hidden" />
+					</div>
+				</div>
+			</div>
+			<div className="mt-[100px]">
+				<div className="sm:text-[40px] text-[30px] text-center text-[#37405E]">
+					Own Your Dream Home – Act Fast!
+				</div>
+				<div className="text-[#7b7b7b] mt-[30px] flex flex-col gap-[20px]">
+					{dreamHomeItems.map((dreamHomeItem, index) => (
+						<div className="flex sm:items-center items-start gap-[20px] ">
+							<img className="w-[40px]" src={dreamHomeItem.url} />
+							<div>{dreamHomeItem.description}</div>
+						</div>
+					))}
+				</div>
+			</div>
+			<div className="my-[100px]">
+				<div className="sm:text-[40px] text-[30px] text-center text-[#37405E]">
+					Fair Field – Where Luxury Meets Smart Investment!
+				</div>
+				<div className="bg-[#37405E] h-fit flex items-center py-[10px] px-[20px] rounded-[5px] cursor-pointer sm:text-[18px] font-medium w-fit mx-auto mt-[20px]">
+					<img src={phone} />
+					<span className="text-[#ffffff] ml-[10px]">
+						+91 79 00 88 22 88
+					</span>
+				</div>
+			</div>
+		</div>
+	)
+}
---
+++ src/components/Variables.js
@@ -0,0 +1,186 @@
+import strategicLocation from '../images/base-features/strategic-location.svg'
+import luxuryVilla from '../images/base-features/luxury-villa.svg'
+import cctv from '../images/base-features/cctv.svg'
+import smartInfrastructure from '../images/base-features/smart-infrastructure.svg'
+import highProperty from '../images/base-features/high-property.svg'
+import loan from '../images/base-features/loan.svg'
+
+import garden from '../images/amenities/garden.svg'
+import clubHouse from '../images/amenities/club-house.svg'
+import sports from '../images/amenities/sports.svg'
+import webcam from '../images/amenities/webcam.svg'
+import roads from '../images/amenities/roads.svg'
+
+import travel from '../images/locations/travel.svg'
+import school from '../images/locations/school.svg'
+import college from '../images/locations/college.svg'
+import restaurant from '../images/locations/restaurant.svg'
+import superMarket from '../images/locations/super-market.svg'
+import hospital from '../images/locations/hospital.svg'
+import bank from '../images/locations/bank.svg'
+
+import discounts from '../images/dream-home/discounts.svg'
+import priceRaise from '../images/dream-home/price-raise.svg'
+import sold from '../images/dream-home/sold.svg'
+
+export const basicFeatures = [
+	{
+		url: strategicLocation,
+		title: 'Strategic Location',
+		description: 'Situated in a well-connected neighborhood, offering easy access to business districts, educational institutions, and healthcare facilities. Live in a thriving area that combines convenience with future growth potential',
+		customStyle: 'broder-solid border-[1px] border-[#BFBDBD]',
+	},
+	{
+		url: luxuryVilla,
+		title: 'Luxury Villas & Premium Plots',
+		description: 'Choose from a range of customizable homes and plots designed to fit your lifestyle needs, starting from ₹18L. Build a home that reflects your personality and meets your every requirement.',
+		customStyle: 'broder-solid border-[1px] border-[#BFBDBD]',
+	},
+	{
+		url: cctv,
+		title: '24/7 Gated Security & CCTV Surveillance',
+		description: 'Experience complete peace of mind with round-the-clock security personnel and advanced monitoring systems. Your safety and privacy are our top priorities',
+		customStyle: 'broder-solid border-[1px] border-[#BFBDBD]',
+	},
+	{
+		url: smartInfrastructure,
+		title: 'Eco-Friendly & Smart Infrastructure',
+		description: 'Benefit from an environment-friendly community featuring solar lighting, rainwater harvesting, and extensive green spaces. Live sustainably while enjoying modern, energy-efficient amenities.',
+		customStyle: 'broder-solid border-[1px] border-[#BFBDBD]',
+	},
+	{
+		url: highProperty,
+		title: 'High Property Appreciation',
+		description: 'nefit from an environment-friendly community featuring solar',
+		customStyle: 'broder-solid border-[1px] border-[#BFBDBD]',
+	},
+	{
+		url: loan,
+		title: 'Flexible Payment Plans & Loan Assistance',
+		description: 'Enjoy an effortless home-buying experience with multiple payment options and easy financing solutions. Our team will assist you in finding the best financial plan suited to your needs.',
+		customStyle: 'broder-solid border-[1px] border-[#BFBDBD]',
+	},
+]
+
+export const premiumAmenities = [
+	{
+		url: garden,
+		title: 'Spacious Green Campus & Well made Tree Plantation',
+		description: 'Stroll through beautifully designed green spaces, offering a tranquil escape from the hustle of city life. Relax in nature while enjoying the fresh air and scenic views.',
+		customStyle: '',
+	},
+	{
+		url: webcam,
+		title: 'Advanced Security Features',
+		description: 'Our gated community ensures complete safety with restricted access, CCTV surveillance, and trained security personnel. Your family’s  well-being is our highest priority.',
+		customStyle: '',
+	},
+	{
+		url: roads,
+		title: 'Well-Planned Roads & Parking Spaces',
+		description: 'Navigate easily with well-maintained internal roads and ample parking space. Designed for seamless movement, ensuring convenience for all residents and visitors.',
+		customStyle: '',
+	},
+]
+
+export const locationList = [
+	{
+		url: travel,
+		title: 'Airport',
+		items: [
+			'Coimbatore International Airport - 11.3 Km',
+			'Coimbatore Junction - 5.7 Km',
+			'Podanur Junction - 3.5 Km',
+		]
+	},
+	{
+		url: school,
+		title: 'Schools',
+		items: [
+			'Trinity Matriculation School - 2.8 Km',
+			'Alvernia Matriculation School - 2.6 Km',
+			'BVM School - 2 Km',
+			'Vidhya Nikethan Public School - 4.3 Km',
+		]
+	},
+	{
+		url: college,
+		title: 'Colleges',
+		items: [
+			'Nirmala Women\'s College - 3.8 Km',
+			'PSGR Krishnammal Girls College - 7.5 Km',
+			'PSG College of Technology - 6.7 Km',
+			'PSG Arts & Science College (Sitra) - 9.9 Km',
+			'Bishop Appasamy College - 5.3 Km',
+			'Bishop Ambrose College - 4.5 Km',
+			'Krishna College - 10 Km',
+		]
+	},
+	{
+		url: restaurant,
+		title: 'Restaurants',
+		items: [
+			'Thinai Amuthu Millet Restaurant - 750 meters',
+			'BBH (Bangalore Biriyani Hotel) Restaurant & Cafe - 4.2 Km',
+			'Nanjai Virundhu Restaurant  - 5.1 Km',
+			'Shree Anandas Restaurant - 4.7 Km',
+			'Denmark Drive-in Restaurant - 6.7 Km',
+		]
+	},
+	{
+		url: superMarket,
+		title: 'Grocery & Hypermarkets',
+		items: [
+			'Dennis Hyper Market - 3.3 Km',
+			'Lulu Hyper Market - 5 Km',
+			'Shri Kannan Departmental Store - 1.2 Km',
+		]
+	},
+	{
+		url: hospital,
+		title: 'Hospitals Nearby',
+		items: [
+			'SLV Hospital - 900 Meter',
+			'Sri Bala Medical Centre - 2.3 Km',
+			'Gem Hospital - 2.2 Km',
+			'GKNM Hospital - 5.4 Km',
+			'PSG Hospital - 7 Km',
+			'KG Hospital - 5. 6 Km',
+			'Ortho One Hospital - 3.2 Km',
+		]
+	},
+	{
+		url: bank,
+		title: 'Banks Nearby',
+		items: [
+			'Bank Of Baroda - 1.2 Km',
+			'Indian Overseas Bank - 1.4 Km',
+			'State Bank of India - 1.5 Km',
+			'Axis Bank - 2.4 Km',
+			'ICICI Bank - 2.6 Km',
+			'HDFC Bank - 2.8 Km',
+			'Canara Bank - 2.9 Km',
+			'Indian Bank - 2.4 Km',
+			'IDBI  - 2.3 Km',
+			'Karur Vysya Bank - 2.5 Km',
+			'Indusind Bank - 2.5 Km',
+			'Andhra Bank - 3 Km',
+		]
+	},
+
+]
+
+export const dreamHomeItems = [
+	{
+		url: sold,
+		description: 'Over 32 plots already sold – Limited units available! Secure your dream home before it\'s too late.'
+	},
+	{
+		url: priceRaise,
+		description: 'Prices Set to Increase Soon – Real estate values are rising rapidly, making now the best time to invest. Don\'t miss out on this opportunity!'
+	},
+	{
+		url: discounts,
+		description: 'Festive Discounts & Special Offers Available! Make your purchase even more rewarding with exclusive seasonal benefits.'
+	},
+]
